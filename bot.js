1c058d8dbfba56f2498795231063e8b55832273b
mazenmezo11 <46426920+mazenmezo11@users.noreply.github.com>
2019-01-07 18:39:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
        
-client.on('guildMemberAdd', Sal => { //By Salto7#4595
+client.on('guildMemberAdd', member => { 
     var embed = new Discord.RichEmbed()
-    .setAuthor(Sal.user.username, Sal.user.avatarURL)
-    .setThumbnail(Sal.user.avatarURL)
-    .setImage('https://cdn.discordapp.com/attachments/492862340484694027/493771573740830740/welcome1.png') //هنا حط الصوره الي تبيها
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setImage('https://cdn.discordapp.com/attachments/492862340484694027/493771573740830740/welcome1.png')
     .setTitle('عضو جديد!')
     .setDescription('مرحبا بك بالسيرفر')
-    .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
-    .addField('``تاق العضو``', Sal.user.discriminator, true)
-    .addField('``تم الانشاء في``', Sal.user.createdAt, true)
-    .addField(' 👤  انت رقم',`**[ ${Sal.guild.memberCount} ]**`,true)
+    .addField('``ايدي العضو``:',"" +  member.user.id, true)
+    .addField('``تاق العضو``', member.user.discriminator, true)
+    .addField('``تم الانشاء في``', member.user.createdAt, true)
+    .addField(' 👤  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
     .setColor('RANDOM')
     .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
-    var channel =Sal.guild.channels.find('name', ' (welcome)') // هنا حط اسم الروم الي تبيه يكتب فيه
+    var channel =member.guild.channels.find('name', ' (welcome)')
     if (!channel) return;
     channel.send({embed : embed});
     });
