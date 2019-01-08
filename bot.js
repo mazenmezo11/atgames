58df44e06bebcc842d28de6ab68fe1586b82c1e7
mazenmezo11 <46426920+mazenmezo11@users.noreply.github.com>
2019-01-08 03:19:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,8 +78,64 @@ client.on('message', message=> {
     if (message.author.bot) return;
     if (message.isMentioned(client.user))
     {//Toxic Codes
-    message.reply(" أنا مجرد روبوت. ولاكن أخبرنى ماذا تحتاج؟");
+    message.reply("لا أستطيع المساعدة.. أنا مجرد روبوت :)");
     }
 });
+
+client.on("message", (message) => {//Toxic Codes
+   if (message.content.startsWith("#new")) {//Toxic Codes  
+        const reason = message.content.split(" ").slice(1).join(" ");    
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {//Toxic Codes
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {//Toxic Codes
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });    
+            c.overwritePermissions(role2, {//Toxic Codes
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {//Toxic Codes
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: **تم إنشاء تذكرتك ، #${c.name}.**`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`مرحباّ ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم لدينا قريبا للمساعدة.`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+ 
+  if (message.content.startsWith("*close")) {//Toxic Codes
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+       message.channel.send(`هل أنت متأكد؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء!\n للتأكيد ، اكتب\`*confirm\`. سيؤدي ذلك إلى مهلة زمنية في غضون 10 ثوانٍ وإلغائها`)
+           .then((m) => {
+               message.channel.awaitMessages(response => response.content === '*confirm', {
+                       max: 1,
+                       time: 10000,
+                       errors: ['time'],
+                   })  
+                   .then((collected) => {
+                       message.channel.delete();
+                   })    
+                   .catch(() => {
+                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                           m2.delete();
+                       }, 3000);
+                   });
+           });
+   }
+ 
+});
+
 
 client.login('NTMxODU0MzM1OTkxMDg3MTA3.DxUH5w.Er9zZouaPrzngzlCQeR3MKVsZjs');
