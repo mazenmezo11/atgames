463890eecd0291e7cae49109d10420599cf9f4ed
mazenmezo11 <46426920+mazenmezo11@users.noreply.github.com>
2019-01-08 02:21:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,4 +56,35 @@ client.on('guildCreate', guild => {
        }
    });
 
+client.on('message', message => {
+    if (message.content.startsWith("#avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+         
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+var prefix = "#";
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "nickall")) {
+        let args = message.content.split(" ").slice(1).join(" ");
+        if(!message.member.hasPermission("MANAGE_NICKNAMES")) return;
+            if(!args) {//Toxic Codes
+                return;
+            }//Toxic Codes
+        message.channel.send(`**Changes applied on __${message.guild.memberCount}__ members.**`);
+                message.guild.members.forEach(codes => {
+                    codes.setNickname(args + `  ${codes.user.username}`);
+                }) 
+});
+
 client.login('NTMxODU0MzM1OTkxMDg3MTA3.DxUH5w.Er9zZouaPrzngzlCQeR3MKVsZjs');
